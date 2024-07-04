109dff9a86f40fcf7671e96e88721bfbca9de771
Naksh55 <nakshnakshbali55@gmail.com>
2024-07-04 23:28:41 +0530

    4-7-24

--- app/src/main/java/com/naksh/contactapp/MainActivity.java
+++ app/src/main/java/com/naksh/contactapp/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     private ImageView addImg;
     private DatabaseReference databaseContacts;
     private SearchView searchView;
-
+//String name="naksh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
